f884a74819f56769c2f43e153dd821cc7ca99ac5
Noronha <inc.691@gmail.com>
2020-08-22 15:38:51 -0300

    Exercise 14 included

--- Tests/test.c
+++ Tests/test.c
@@ -1,13 +1,18 @@
 #include "stdio.h"
+#include "ctype.h"
 
 int main() {
+        char wantToBuyMore;
 
-    if ( 1 == 1 && 1 != 1 || 1 == 1 && 1 != 1 || 1 == 1 && 1 != 1 ) {
-        printf("true\n");
-    }
-    else {
-        printf("false\n");
-    }
+        while(1) {
+            printf("Do you want to buy something else? [Y/N]");
+            wantToBuyMore = tolower(getchar());
+            getchar();
 
+            if ( wantToBuyMore == 'y' || wantToBuyMore == 'n' ) break;
+            printf("Select a valid option!\n");
+        }
+
+        printf("\nOutside\n");
     return 0;
 }
